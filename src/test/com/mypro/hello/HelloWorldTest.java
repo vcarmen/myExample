4606d3e503b199f767fe57ee326cb0a744748d46
EC2 Default User <ec2-user@ip-172-31-27-250.ec2.internal>
2018-11-13 03:19:36 +0000

    Initial Commit

---
+++ src/test/com/mypro/hello/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package main.com.mypro.hello;
+import com.mypro.hello.HelloWorld;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test public void testHelloWorldMessage() {
+        HelloWorld classUnderTest = new HelloWorld();
+        assertNotNull("Hello", "Hello");
+    }
+}
+
